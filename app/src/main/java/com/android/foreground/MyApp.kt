075e058f4d92bb83636ea3f5442a0ebff0c036bb
Danijel Vrecic <danijel.vrecic@hub.si>
2022-01-11 11:03:58 +0100

    Create notification channel for foreground service.

--- app/src/main/java/com/android/foreground/MyApp.kt
+++ app/src/main/java/com/android/foreground/MyApp.kt
@@ -1,11 +1,24 @@
 package com.android.foreground
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import androidx.core.content.getSystemService
 
 class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel()
         startForegroundService(DummyForegroundService.getCallingIntent(this))
+    }
+
+    private fun createNotificationChannel() {
+        val serviceChannel = NotificationChannel(
+            DummyForegroundService.CHANNEL_ID,
+            "dummy_foreground_service_channel_01",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        getSystemService<NotificationManager>()?.createNotificationChannel(serviceChannel)
     }
 }
